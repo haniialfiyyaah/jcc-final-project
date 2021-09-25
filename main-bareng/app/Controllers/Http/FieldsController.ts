6a30c6fdb59d47b293cf0a993a42d698265fa352
haniialfiyyaah <hanii.alfiyyah@gmail.com>
2021-09-25 12:38:05 +0700

    Update field get response

--- main-bareng/app/Controllers/Http/FieldsController.ts
+++ main-bareng/app/Controllers/Http/FieldsController.ts
@@ -9,10 +9,6 @@ export default class FieldsController {
     return field.serialize({
       fields: { omit: ['venue_id'] },
       relations: {
-        venue: {
-          fields: ['created_at', 'updated_at', 'user_id'],
-          relations: { owner: { fields: ['id', 'name', 'email'] } },
-        },
         bookings: {
           fields: {
             omit: ['created_at', 'updated_at', 'field_id', 'user_id_booking'],
@@ -26,19 +22,26 @@ export default class FieldsController {
   public async index({ params, request, response }: HttpContextContract) {
     const { venue_id } = params
     const { name, type } = request.qs()
-    await Venue.findOrFail(venue_id) // not found error
+
+    const venue = await Venue.findOrFail(venue_id)
+    await venue.load('user') // not found error
+    const venueJSON = venue.serialize({
+      fields: { omit: ['created_at', 'updated_at', 'user_id'] },
+      relations: { owner: { fields: ['id', 'name', 'email'] } },
+    })
+
     const fields = await Field.query()
       .where('venue_id', venue_id)
       .where('name', 'like', `%${name || ''}%`)
       .where('type', 'like', `%${type || ''}%`)
-      .preload('venue', (venue) => {
-        venue.preload('user')
-      })
       .preload('bookings', (booking) => {
         booking.preload('user')
       })
     const fieldsJSON = fields.map((field) => this.serializeField(field))
-    response.ok({ message: 'Success.', data: fieldsJSON })
+    response.ok({
+      message: 'Success.',
+      data: { ...venueJSON, fields: fieldsJSON },
+    })
   }
 
   public async store({ params, request, response }: HttpContextContract) {
@@ -51,19 +54,26 @@ export default class FieldsController {
 
   public async show({ params, response }: HttpContextContract) {
     const { venue_id, id } = params
+    const venue = await Venue.findOrFail(venue_id)
+    await venue.load('user') // not found error
+    const venueJSON = venue.serialize({
+      fields: { omit: ['created_at', 'updated_at', 'user_id'] },
+      relations: { owner: { fields: ['id', 'name', 'email'] } },
+    })
+
     const field = await Field.query()
       .where('id', id)
       .where('venue_id', venue_id)
-      .preload('venue', (venue) => {
-        venue.preload('user')
-      })
       .preload('bookings', (booking) => {
         booking.preload('user')
       })
       .firstOrFail()
     //  select fields
     const fieldJSON = this.serializeField(field)
-    response.ok({ message: 'Success.', data: fieldJSON })
+    response.ok({
+      message: 'Success.',
+      data: { ...venueJSON, fields: fieldJSON },
+    })
   }
 
   public async update({ params, request, response }: HttpContextContract) {
